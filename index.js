ab31ff8b0a1b5d722f2e06af92841d8dd0f49df4
Heiko Mamerow <mail@heikomamerow.de>
2016-08-01 18:33:03 +0200

    Debug: max-len, camlecase, quotes

--- index.js
+++ index.js
@@ -8,23 +8,23 @@ module.exports = postcss.plugin( 'postcss-table-of-contents', function () {
         // Variable for match counting
         var i = [ ];
 
-        // Traverses the container’s descendant nodes, calling callback for each comment node.
+        // Traverses the container’s descendant nodes
         css.walkComments( function ( comment ) {
 
             // Get all comments as string
-            var comment_string = comment.toString();
+            var String = comment.toString();
 
-            // Replace pattern with one ore more charakters with our standard pattern => #)
-            var comment_string_replaced_tmp = comment_string.replace( /(\S+)\)/, "#)" );
+            // Replace pattern with one ore more charakters
+            var StringMatchTmp = String.replace( /(\S+)\)/, '#)' );
 
             // Replace # with increment number
-            var comment_string_replaced = comment_string_replaced_tmp.replace( "#)", function ( m, p1 ) {
+            var StringMatch = StringMatchTmp.replace( '#)', function ( m, p1 ) {
                 i[p1] = ( i[p1] || 0 ) + 1;
-                return i[p1].toString() + ")";
+                return i[p1].toString() + ')';
             } );
 
             // Replace all comments with our new string
-            comment.replaceWith( comment_string_replaced );
+            comment.replaceWith( StringMatch );
 
         } );
 
