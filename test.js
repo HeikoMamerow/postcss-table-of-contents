8d236fa80ec8e4d4fa0277c4cc536155a80fe69b
Heiko Mamerow <mail@heikomamerow.de>
2016-08-01 18:36:45 +0200

    Debug: indent, eol

--- test.js
+++ test.js
@@ -1,16 +1,17 @@
 import postcss from 'postcss';
-    import test    from 'ava';
-    import plugin from './';
-    function run( t, input, output, opts = { } ) {
-    return postcss( [ plugin( opts ) ] ).process( input )
-        .then( function ( result ) {
-        t.deepEqual( result.css, output );
-            t.deepEqual( result.warnings( ).length, 0 );
-        } );
-    }
+import test from 'ava';
+import plugin from './';
+
+function run(t, input, output, opts = {}) {
+    return postcss([plugin(opts)]).process(input)
+        .then(function(result) {
+            t.deepEqual(result.css, output);
+            t.deepEqual(result.warnings().length, 0);
+        });
+}
 
 // Test replacing
 
-    test( 'replace pattern from #) to 1)', function ( t ) {
-    return run( t, '/* #) */', '/* 1) */', { } );
-    } );
+test('replace pattern from #) to 1)', function(t) {
+    return run(t, '/* #) */', '/* 1) */', {});
+});
